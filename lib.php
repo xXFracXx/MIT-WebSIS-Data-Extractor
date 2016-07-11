24e6c22033b959cfffb3fd71c2da1bfe8bad8562
Shashank Pincha <xxfracxx@gmail.com>
2016-07-11 16:41:13 +0400

    Test routes (2)

--- lib.php
+++ lib.php
@@ -51,6 +51,15 @@ function post_data($site,$data){
     unset($datapost);
 }
 
+function getCurrentUri()
+{
+    $basepath = implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)) . '/';
+    $uri = substr($_SERVER['REQUEST_URI'], strlen($basepath));
+    if (strstr($uri, '?')) $uri = substr($uri, 0, strpos($uri, '?'));
+    $uri = '/' . trim($uri, '/');
+    return $uri;
+}
+
 define('HDOM_TYPE_ELEMENT', 1);
 define('HDOM_TYPE_COMMENT', 2);
 define('HDOM_TYPE_TEXT',    3);
