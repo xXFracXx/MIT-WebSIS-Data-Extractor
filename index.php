e3816e477cf57acdc70591745297b9c5eb43cb17
Shashank Pincha <xxfracxx@gmail.com>
2016-07-11 18:25:36 +0400

    Testing

--- index.php
+++ index.php
@@ -12,7 +12,28 @@ foreach($routes as $route) {
 //Now, $routes will contain all the routes. $routes[0] will correspond to first route. For e.g. in above example $routes[0] is search, $routes[1] is book and $routes[2] is fitzgerald
 
 if($routes[1] == "test") {
-    header('Location: test.php');
+    function pg_connection_string_from_database_url() {
+      extract(parse_url($_ENV["DATABASE_URL"]));
+      return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+    }
+    # Here we establish the connection. Yes, that's all.
+    $conn = pg_connection_string_from_database_url();
+    $pg_conn = pg_connect($conn);
+    # Now let's use the connection for something silly just to prove it works:
+    $result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+    print "<pre>\n";
+    if (!pg_num_rows($result)) {
+      print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+    } else {
+      print "Tables in your database:\n";
+      while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+    }
+    print "\n";
+
+    echo $_ENV["DATABASE_URL"];
+    echo "hello world";
+
+    exit();
 }
 
 $student_id = (string)$routes[1];
