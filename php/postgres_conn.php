66df8a26c487f94c9c8b66d82d4bc9b503dbe14f
Shashank Pincha <xxfracxx@gmail.com>
2016-07-11 19:29:47 +0400

    Testing (PG DB)

--- php/postgres_conn.php
+++ php/postgres_conn.php
@@ -1,11 +1,8 @@
 <?php
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["DATABASE_URL"]));
+  var_dump(parse_url($_ENV["DATABASE_URL"]));
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-  echo $user;
-  echo $pass;
-  echo $host;
-  echo $path;
 }
 
 $conn = pg_connection_string_from_database_url();
