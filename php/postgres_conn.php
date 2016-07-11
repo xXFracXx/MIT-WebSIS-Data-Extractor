afff569203bc9a5aac3e1fe97b82db5fe37a2a99
Shashank Pincha <xxfracxx@gmail.com>
2016-07-11 19:12:19 +0400

    Testing (PG DB)

--- php/postgres_conn.php
+++ php/postgres_conn.php
@@ -8,7 +8,7 @@ $conn = pg_connection_string_from_database_url();
 $pg_conn = pg_connect($conn);
 
 function test_pg_conn() {
-    $result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+    $result = pg_query($pg_conn, "SELECT * FROM student_info");
     print "<pre>\n";
     if (!pg_num_rows($result)) {
       print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
