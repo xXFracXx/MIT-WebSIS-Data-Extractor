57a5322936146117dfe5fc39d878e1e740f54c5b
Shashank Pincha <xxfracxx@gmail.com>
2016-07-11 19:52:02 +0400

    Testing (PG DB)

--- php/postgres_conn.php
+++ php/postgres_conn.php
@@ -7,6 +7,7 @@ function pg_connection_string_from_database_url() {
   $port = parse_url($url, PHP_URL_PORT);
   $db_temp = parse_url($url, PHP_URL_PATH);
   $db = ltrim($db_temp, '/');
+  echo $user." ".$pass." ".$host." ".$port." "$db;
   return "user=$user password=$pass host=$host port=$port dbname=$db";
 }
 
