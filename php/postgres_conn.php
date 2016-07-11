3a41775e2587a7b1e339a0d2bbf689154671d1bb
Shashank Pincha <xxfracxx@gmail.com>
2016-07-11 21:34:40 +0400

    Add $row[1]

--- php/postgres_conn.php
+++ php/postgres_conn.php
@@ -23,7 +23,7 @@ function test_pg_conn() {
       print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
     } else {
       print "Tables in your database:\n";
-      while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+      while ($row = pg_fetch_row($result)) { print("- $row[0] $row[1]\n"); }
     }
     print "\n</pre>";
 }
