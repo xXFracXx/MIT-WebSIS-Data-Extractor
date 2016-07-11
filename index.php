b9be8825ed33a9336b5539c4fc7b86cda5f9a65c
Shashank Pincha <xxfracxx@gmail.com>
2016-07-11 17:32:41 +0400

    Final Testing

--- index.php
+++ index.php
@@ -11,10 +11,10 @@ foreach($routes as $route) {
 }
 //Now, $routes will contain all the routes. $routes[0] will correspond to first route. For e.g. in above example $routes[0] is search, $routes[1] is book and $routes[2] is fitzgerald
 
-$student_id = (string)$routes[0];
-$student_dob = (string)$routes[1];
+$student_id = (string)$routes[1];
+$student_dob = (string)$routes[2];
 
-$post_cred = "idValue=".$routes[1]."&birthDate_i18n=".$routes[2]."&birthDate=".$routes[2];
+$post_cred = "idValue=".$student_id."&birthDate_i18n=".$student_dob."&birthDate=".$student_dob;
 
 $login_url = "http://websismit.manipal.edu/websis/control/createAnonSession";
 
@@ -26,24 +26,21 @@ $student_latest_enrollment = "http://websismit.manipal.edu/websis/control/ListCT
 $data_page = grab_page($student_latest_enrollment); //echo $page;
 $data_html = str_get_html($data_page);
 
-if($routes[2] == "marks") {
-    if($routes[3] == "IA1") {
+if($routes[3] == "marks") {
+    if($routes[4] == "IA1") {
         $data = get_IA1_data($data_html);
-    } else if($routes[3] == "IA2") {
+    } else if($routes[4] == "IA2") {
         $data = get_IA2_data($data_html);
-    } else if($routes[3] == "IA3") {
+    } else if($routes[4] == "IA3") {
         $data = get_IA3_data($data_html);
     }
-} else if($routes[2] == "attendance") {
+} else if($routes[3] == "attendance") {
     $data = get_attendance_data($data_html);
-} else if($routes[2] == "course") {
+} else if($routes[3] == "course") {
     $data = get_course_data($data_html);
 }
 
-echo $base_url;
-echo $post_cred;
-
-$data = get_course_data($data_html);
+echo $routes[0];
 
 $json = json_encode($data, JSON_PRETTY_PRINT);
 printf('<pre>%s</pre>', $json)
