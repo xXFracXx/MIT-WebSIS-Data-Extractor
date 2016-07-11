3d7e9d29b269f25484c71c51e8ef06c8082d8483
Shashank Pincha <xxfracxx@gmail.com>
2016-07-11 16:20:03 +0400

    Change output

--- index.php
+++ index.php
@@ -23,17 +23,16 @@ $IA1_data = get_IA1_data($data_html);
 $IA2_data = get_IA2_data($data_html);
 $IA3_data = get_IA3_data($data_html);
 
-echo json_encode($c_data);
-echo nl2br("\n\n\n");
-echo json_encode($a_data);
-echo nl2br("\n\n\n");
-echo json_encode($IA1_data);
-echo nl2br("\n\n\n");
-echo json_encode($IA2_data);
-echo nl2br("\n\n\n");
-echo json_encode($IA3_data);
+// echo json_encode($c_data);
+// echo nl2br("\n\n\n");
+// echo json_encode($a_data);
+// echo nl2br("\n\n\n");
+// echo json_encode($IA1_data);
+// echo nl2br("\n\n\n");
+// echo json_encode($IA2_data);
+// echo nl2br("\n\n\n");
+// echo json_encode($IA3_data);
 
-//$temp_c_data = str_replace('$nbsp;', '', $c_data);
-//json = json_encode($c_data, JSON_PRETTY_PRINT);
-//printf('<pre>%s</pre>', $json)
+$json = json_encode($c_data, JSON_PRETTY_PRINT);
+printf('<pre>%s</pre>', $json)
 ?>
