f1c4a1f0d3b44510cf220278044eac69b5f3793e
Shashank Pincha <xxfracxx@gmail.com>
2016-07-12 17:48:11 +0400

    Testing

--- php/postgres_conn.php
+++ php/postgres_conn.php
@@ -49,9 +49,9 @@ function uploadToDB($data, $id, $requested_sem, $col) {
     $new_info[$db_sem] = $data;
     var_dump($old_info);echo nl2br("\n\n\n");
     var_dump($new_info);echo nl2br("\n\n\n");
-    $final_info = array_merge($new_info, $old_info);
-    var_dump($final_info);
+    $final_info = array_merge($old_info, $new_info);
     $final_info[$db_sem]["timestamp"] = date("Y/m/d h:i:sa");
+    var_dump($final_info);
     $json = json_encode($final_info);
     $conn = pg_connection_string_from_database_url();
     $pg_conn = pg_connect($conn);
